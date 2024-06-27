81b9483a040288d8902c1cf7246363fe9a0e217f
AswaniBolisetti <aswanibolisetti@gmail.com>
2024-06-27 20:13:34 +0530

    for testing purpose

--- src/routes/(tools)/whiteboard/+page.svelte
+++ src/routes/(tools)/whiteboard/+page.svelte
@@ -4,6 +4,9 @@
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	<!-- Add tool here -->
+	 WhiteBoard
+	 This is an online whiteboard
+	 
 </div>
 
 <style>
