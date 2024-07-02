c0caf29957c991efec80b24476b6716ace863b81
devishree2305 <devishreenadar@gmail.com>
2024-07-02 18:08:29 +0530

    Update +page.svelte

--- src/routes/(tools)/custom-handwriting-font/+page.svelte
+++ src/routes/(tools)/custom-handwriting-font/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { writable } from "svelte/store";
-  import { saveCharacter, fetchHandwriting } from "./lib/api";
+  import { fetchHandwriting } from "./lib/api";
 
   let showTemplates = true;
   let showMyFonts = false;
@@ -10,12 +10,13 @@
 
   const letters = generateAlphabet();
   let currentIndex = 0;
-  let char = "";
+  let char = letters[currentIndex];
   let previewText = "";
   let drawingCanvas;
   let drawingCtx;
   let text = "";
   let handwriting = writable({});
+  let savedLetters = writable([]);
 
   onMount(() => {
     drawingCtx = drawingCanvas.getContext("2d");
@@ -27,7 +28,20 @@
       });
       handwriting.set(handwritingData);
     });
+    
+    const saved = JSON.parse(localStorage.getItem("savedLetters")) || [];
+    savedLetters.set(saved);
+    saved.forEach((letter) => {
+      const image = localStorage.getItem(letter);
+      handwriting.update((h) => {
+        h[letter] = image;
+        return h;
+      });
+    });
+    updateCanvas(char);
   });
+
+
 
   function generateAlphabet() {
     let result = [];
@@ -40,27 +54,76 @@
   }
 
   const saveHandwriting = async () => {
-    const image = drawingCanvas.toDataURL();
-    await saveCharacter(char, image);
-    drawingCtx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
-    char = "";
+    const image = drawingCanvas.toDataURL("image/png");
+    const letter = letters[currentIndex];
+    localStorage.setItem(letter, image);
+
+    savedLetters.update((saved) => {
+      if (!saved.includes(letter)) {
+        saved.push(letter);
+        localStorage.setItem("savedLetters", JSON.stringify(saved));
+      }
+      return saved;
+    });
+
+    handwriting.update((h) => {
+      h[letter] = image;
+      return h;
+    });
   };
+
+  function deleteLetter(letter) {
+    savedLetters.update((saved) => {
+      const index = saved.indexOf(letter);
+      if (index !== -1) {
+        saved.splice(index, 1);
+        localStorage.removeItem(letter);
+        localStorage.setItem("savedLetters", JSON.stringify(saved));
+      }
+      return saved;
+    });
+
+    handwriting.update((h) => {
+      delete h[letter];
+      return h;
+    });
+
+    if (letter === char) {
+      updateCanvas(char);
+    }
+  }
 
   function updatePreview(letter) {
     previewText += letter;
   }
 
   function nextLetter() {
-    currentIndex = (currentIndex + 1) % letters.length;
-    previewText = "";
-    char = letters[currentIndex];
-  }
+  currentIndex = (currentIndex + 1) % letters.length;
+  previewText = "";
+  char = letters[currentIndex];
+  updateCanvas(char);
+}
 
-  function prevLetter() {
-    currentIndex = (currentIndex - 1 + letters.length) % letters.length;
-    previewText = "";
-    char = letters[currentIndex];
+function prevLetter() {
+  currentIndex = (currentIndex - 1 + letters.length) % letters.length;
+  previewText = "";
+  char = letters[currentIndex];
+  updateCanvas(char);
+}
+
+function updateCanvas(letter) {
+  const image = localStorage.getItem(letter);
+  if (image) {
+    const img = new Image();
+    img.onload = () => {
+      drawingCtx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+      drawingCtx.drawImage(img, 0, 0);
+    };
+    img.src = image;
+  } else {
+    drawingCtx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
   }
+}
 
   function startDrawing() {
     drawingCtx.strokeStyle = "#000";
@@ -92,6 +155,13 @@
     isDrawing = false;
     drawingCtx.closePath();
   }
+
+  const downloadCanvas = () => {
+    const link = document.createElement("a");
+    link.download = `${char}.png`;
+    link.href = drawingCanvas.toDataURL("image/png");
+    link.click();
+  };
 
   const copyText = () => {
     const container = document.createElement("div");
@@ -273,13 +343,31 @@
         on:click={() => updatePreview(letters[currentIndex])}
       >
         {letters[currentIndex]}
-      </div>
-    </div>
+        
+     </div>
+    <button 
+      class="flex-none"
+      on:click={() => deleteLetter(letters[currentIndex])}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="14"
+        width="12.25"
+        viewBox="0 0 448 512"
+      >
+        <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+        <path
+          fill="#ff0000"
+          d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"
+        />
+      </svg>
+    </button>
+  </div>
     <div class="drawing-container">
       <canvas
         class="drawing-canvas"
         bind:this={drawingCanvas}
-        width="800"
+        width="850"
         height="300"
       >
         Your browser does not support the HTML5 canvas tag.
@@ -298,6 +386,10 @@
         class="cursor-pointer m-2 bg-blue-600 text-sm hover:bg-green-600 text-white px-6 py-4 rounded-lg"
         on:click={saveHandwriting}>Save</button
       >
+    
+      <button 
+        class="cursor-pointer m-2 bg-blue-600 text-sm hover:bg-green-600 text-white px-6 py-4 rounded-lg" 
+        on:click={downloadCanvas}>Download PNG</button>
     </div>
   </div>
 </section>
