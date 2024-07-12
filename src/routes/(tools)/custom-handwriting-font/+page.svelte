748a89906b3eab7fb839b14ddb7b5f2d59166ca7
Riya Kushwaha <riyakushwaha253@gmail.com>
2024-07-12 22:03:16 +0530

    updated ui and removed the unnecessary part

--- src/routes/(tools)/custom-handwriting-font/+page.svelte
+++ src/routes/(tools)/custom-handwriting-font/+page.svelte
@@ -2,7 +2,6 @@
   import { onMount } from "svelte";
   import { writable } from "svelte/store";
   import ImageTracer from "imagetracerjs";
-  import { createFontFromSVGs } from "$lib/fontUtils";
 
   let letters = Object.keys(generateAlphabet());
   let currentIndex = 0;
@@ -15,47 +14,6 @@
   let isDrawing = false;
   let penWidth = 2;
   let eraserWidth = 10;
-  let fonts = writable([]); // Store for dynamic fonts
-
-  function handleCreateFont() {
-    createFontFromSVGs();
-  }
-
-  const handleDownload = () => {
-    const a4Width = 595;
-    const a4Height = 842;
-    const canvas = document.createElement("canvas");
-    canvas.width = a4Width;
-    canvas.height = a4Height;
-    const ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#000000";
-    ctx.font = "14px Arial";
-    ctx.textAlign = "center";
-
-    const headingOffsetY = 5;
-    const boxWidth = 60;
-    const boxHeight = 80;
-    const marginY = 20;
-    const marginX = 10;
-    const startX = 20;
-    const startY = 20;
-
-    letters.forEach((letter, index) => {
-      const x = startX + (index % 8) * (boxWidth + marginX);
-      const y = startY + Math.floor(index / 8) * (boxHeight + marginY);
-      ctx.fillText(letter, x + boxWidth / 2, y - headingOffsetY);
-      ctx.strokeRect(x, y, boxWidth, boxHeight);
-    });
-
-    const dataUrl = canvas.toDataURL("image/png");
-    const link = document.createElement("a");
-    link.href = dataUrl;
-    link.download = "handwriting_template.png";
-    link.click();
-  };
 
   onMount(() => {
     drawingCtx = drawingCanvas.getContext("2d");
@@ -231,33 +189,6 @@
     drawingCtx.closePath();
   }
 
-  function addFont() {
-    fonts.update((current) => [
-      ...current,
-      { id: Date.now(), name: "My Font", editing: false },
-    ]);
-  }
-
-  function deleteFont(id) {
-    fonts.update((current) => current.filter((font) => font.id !== id));
-  }
-
-  function editFont(id, newName) {
-    fonts.update((current) =>
-      current.map((font) =>
-        font.id === id ? { ...font, name: newName, editing: false } : font
-      )
-    );
-  }
-
-  function toggleEditFont(id) {
-    fonts.update((current) =>
-      current.map((font) =>
-        font.id === id ? { ...font, editing: !font.editing } : font
-      )
-    );
-  }
-
   async function canvasToSVG() {
     return new Promise((resolve) => {
       const filteredCanvas = document.createElement("canvas");
@@ -331,77 +262,11 @@
   <!-- Sidebar div starts here -->
   <div class="px-6 py-10 h-full">
     <!-- Fonts Collection -->
-    <div class="flex gap-4 pb-4">
+    <div class="pb-4">
       <p class="dark:text-white text-gray-900 text-xl">Fonts Collection</p>
-      <button on:click={addFont}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="20"
-          width="20"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="#63E6BE"
-            d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"
-          />
-        </svg>
-      </button>
-      <button on:click={handleDownload}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="20"
-          width="20"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="#63E6BE"
-            d="M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM127 281c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l71 71L232 136c0-13.3 10.7-24 24-24s24 10.7 24 24l0 182.1 71-71c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 393c-9.4 9.4-24.6 9.4-33.9 0L127 281z"
-          />
-        </svg>
-      </button>
     </div>
 
     <hr class="py-2" />
-
-    <!-- My Fonts -->
-    <div
-      class="text-gray-900 text-sm px-4 py-2 dark:text-white flex flex-col w-auto"
-    >
-      <ul>
-        {#each $fonts as font}
-          <li class="mb-2 flex items-center">
-            {#if font.editing}
-              <input
-                type="text"
-                class="px-2 py-1 w-full border dark:bg-gray-700 dark:text-white"
-                bind:value={font.name}
-                on:blur={() => editFont(font.id, font.name)}
-                on:keydown={(e) =>
-                  e.key === "Enter" && editFont(font.id, font.name)}
-              />
-            {:else}
-              <span
-                class="ml-2 cursor-pointer"
-                on:dblclick={() => toggleEditFont(font.id)}>{font.name}</span
-              >
-            {/if}
-            <button class="flex-none ml-2" on:click={() => deleteFont(font.id)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="14"
-                width="12.25"
-                viewBox="0 0 448 512"
-              >
-                <path
-                  fill="#ff0000"
-                  d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"
-                />
-              </svg>
-            </button>
-          </li>
-        {/each}
-      </ul>
-    </div>
 
     <!-- Keyboard -->
     <div class="m-4">
@@ -587,18 +452,6 @@
           />
         </svg>
       </button>
-      <button class="text-white" on:click={handleCreateFont}
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="34"
-          width="40"
-          viewBox="0 0 576 512"
-          ><path
-            fill="#00b4cc"
-            d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V428.7c-2.7 1.1-5.4 2-8.2 2.7l-60.1 15c-3 .7-6 1.2-9 1.4c-.9 .1-1.8 .2-2.7 .2H240c-6.1 0-11.6-3.4-14.3-8.8l-8.8-17.7c-1.7-3.4-5.1-5.5-8.8-5.5s-7.2 2.1-8.8 5.5l-8.8 17.7c-2.9 5.9-9.2 9.4-15.7 8.8s-12.1-5.1-13.9-11.3L144 381l-9.8 32.8c-6.1 20.3-24.8 34.2-46 34.2H80c-8.8 0-16-7.2-16-16s7.2-16 16-16h8.2c7.1 0 13.3-4.6 15.3-11.4l14.9-49.5c3.4-11.3 13.8-19.1 25.6-19.1s22.2 7.8 25.6 19.1l11.6 38.6c7.4-6.2 16.8-9.7 26.8-9.7c15.9 0 30.4 9 37.5 23.2l4.4 8.8h8.9c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7L384 203.6V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM549.8 139.7c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM311.9 321c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L512.1 262.7l-71-71L311.9 321z"
-          /></svg
-        ></button
-      >
     </div>
   </div>
 </section>
